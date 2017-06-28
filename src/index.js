774822f1a2c576a967fec8cc9aaf02fa35c7cde6
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-28 13:14:35 +0200

    basic query to API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 //key for GIPHY API
 const API_KEY = '34f1b6a7a8f34603a25e6016b4b15b1f';
+const ROOT_URL = `http://api.giphy.com/v1/gifs/search?q=cat%20dog&api_key=${API_KEY}&limit=25`;
+
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+import $ from "jquery";
 
 import PictureList   from './components/picture_list'; 
 import PictureDetail from './components/picture_detail';
@@ -10,6 +14,17 @@ import PictureDetail from './components/picture_detail';
 class App extends Component{
 	constructor(props){
 		super(props);
+		this.state = { 
+			pictures: [],
+			selectedPicture: null
+		};
+		this.giphySearch('cat%20dog');
+	}
+	giphySearch(term){
+		var xhr = $.get(ROOT_URL);
+		xhr.done(function(data){ 
+			console.log("success got data", data); 
+		});
 	}
 	render(){
 		return (<div><h1>Cats and a few Dogs</h1><PictureList /></div>);
