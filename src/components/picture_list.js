64a7ef2ffff1b0b9fae0f04d4eea5b9401044f74
Jorge Hernández <diego.hernandez@i3television.es>
2017-06-29 12:26:46 +0200

    added masonry dependency, layout applied to list

--- src/components/picture_list.js
+++ src/components/picture_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PictureListItem from './picture_list_item';
+import Masonry from 'react-masonry-component';
 
 const PictureList = ( props ) => {
 
 	const data = props.pictures.data || [];
+
+	const masonryOptions = {
+    	transitionDuration: 0
+	};
 
 	const pictureItems = data.map( (picture)=>{
 		return (
@@ -13,11 +18,24 @@ const PictureList = ( props ) => {
 				picture={picture} />
 			);
 	});
+	/*
 	return (
 		<ul>
 			{pictureItems}
 		</ul>
 	);
+	*/
+	return (
+        <Masonry
+            className={'catsanddogs'} // default ''
+            elementType={'ul'} // default 'div'
+            options={masonryOptions} // default {}
+            disableImagesLoaded={false} // default false
+            updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+        >
+            {pictureItems}
+        </Masonry>
+    );
 };
 
 export default PictureList;
