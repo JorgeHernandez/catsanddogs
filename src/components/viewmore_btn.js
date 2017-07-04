1f8ece28250bcc976a6f7e00e1f38b68851453a0
Jorge Hernández <diego.hernandez@i3television.es>
2017-07-04 14:12:02 +0200

    added id attr

--- src/components/viewmore_btn.js
+++ src/components/viewmore_btn.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const ViewMoreButton = (props) => {
 	return (
-		<a href="#" className="btn" onClick={props.onViewMore}>View more</a>
+		<a href="#" id="viewMore-btn" className="btn" onClick={props.onViewMore}>View more</a>
 	);
 }
 
